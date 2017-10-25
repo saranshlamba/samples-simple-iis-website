fe881e025bb57ec7b4ae79c5b7f0bf335e5818ab
SL <saranshlamba@gmail.com>
2017-10-24 23:10:10 -0700

    zipfile

--- zipfiles.ps1
+++ zipfiles.ps1
@@ -6,5 +6,5 @@ Copy-Item .\default.aspx.cs .\tos3 -force
 Copy-Item .\packages.config .\tos3 -force
 Copy-Item .\readme.md .\tos3 -force
 Copy-Item .\web.config .\tos3 -force
-Copy-Item .\web.config.debug .\tos3 -force
+Copy-Item .\Web.Debug.config .\tos3 -force
 [System.IO.Compression.ZipFile]::CreateFromDirectory(".\tos3", "tos3.zip")
