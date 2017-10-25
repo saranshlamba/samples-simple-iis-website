63c26c010d4f91423efccb4183407a011bbc302f
SL <saranshlamba@gmail.com>
2017-10-24 22:52:45 -0700

    zipfile

--- zipfiles.ps1
+++ zipfiles.ps1
@@ -1,4 +1,4 @@
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 New-Item -ItemType directory -Path .\tos3
 Copy-Item .\default.aspx .\tos3 -force
-[System.IO.Compression.ZipFile]::CreateFromDirectory(".\MGMT-SERVER", "mgmt-server.zip")
+[System.IO.Compression.ZipFile]::CreateFromDirectory(".\tos3", "tos3.zip")
