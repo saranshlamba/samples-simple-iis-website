d2ad35b66bbb731732f38820f7e005f5d8b90c03
SL <saranshlamba@gmail.com>
2017-10-25 15:08:45 -0700

    zipfile

--- zipfiles.ps1
+++ zipfiles.ps1
@@ -1,11 +1,13 @@
 set-executionpolicy unrestricted
 $ErrorActionPreference = "Stop"
 Add-Type -Assembly "System.IO.Compression.FileSystem";
+New-Item -ItemType directory -Path .\webfiles
 New-Item -ItemType directory -Path .\tos3
-Copy-Item .\default.aspx .\tos3 -force
-Copy-Item .\default.aspx.cs .\tos3 -force
-Copy-Item .\packages.config .\tos3 -force
-Copy-Item .\readme.md .\tos3 -force
-Copy-Item .\web.config .\tos3 -force
-Copy-Item .\Web.Debug.config .\tos3 -force
+Copy-Item .\default.aspx .\webfiles -force
+Copy-Item .\default.aspx.cs .\webfiles -force
+Copy-Item .\packages.config .\webfiles -force
+Copy-Item .\readme.md .\webfiles -force
+Copy-Item .\web.config .\webfiles -force
+Copy-Item .\Web.Debug.config .\webfiles -force
+Move-Item .\webfiles .\tos3
 [System.IO.Compression.ZipFile]::CreateFromDirectory(".\tos3", "tos3.zip")
