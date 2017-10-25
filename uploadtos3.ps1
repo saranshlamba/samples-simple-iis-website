538ff3a2bbf110a5c65be82dd6c95e3aa677aee4
SL <saranshlamba@gmail.com>
2017-10-25 16:04:51 -0700

    uploadtos3

--- uploadtos3.ps1
+++ uploadtos3.ps1
@@ -1,2 +1,2 @@
 ﻿set-executionpolicy unrestricted
-Write-S3Object -BucketName s3demo2017 -Key tos3.zip -File .\tos3.zip
+Write-S3Object -BucketName s3demo2017 -Key tos3.zip -File .\tos3.zip -ProfileName slamba -Region us-west-2
