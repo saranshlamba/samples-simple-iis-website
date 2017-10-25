22c8b3a494a995e7c66f8fc70e4cf838bbf4b2a8
SL <saranshlamba@gmail.com>
2017-10-24 23:02:29 -0700

    zipfile

--- zipfiles.ps1
+++ zipfiles.ps1
@@ -2,4 +2,9 @@ set-executionpolicy unrestricted
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 New-Item -ItemType directory -Path .\tos3
 Copy-Item .\default.aspx .\tos3 -force
+Copy-Item .\default.aspx.cs .\tos3 -force
+Copy-Item .\packages.config .\tos3 -force
+Copy-Item .\readme.md .\tos3 -force
+Copy-Item .\web.config .\tos3 -force
+Copy-Item .\web.config.debug .\tos3 -force
 [System.IO.Compression.ZipFile]::CreateFromDirectory(".\tos3", "tos3.zip")
