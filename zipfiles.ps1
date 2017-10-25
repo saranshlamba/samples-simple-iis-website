7a92e96a08fc07bc747339f3ea8559bc91da1b92
SL <saranshlamba@gmail.com>
2017-10-24 23:18:19 -0700

    zipfile

--- zipfiles.ps1
+++ zipfiles.ps1
@@ -2,7 +2,7 @@ set-executionpolicy unrestricted
 $ErrorActionPreference = "Stop"
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 New-Item -ItemType directory -Path .\tos3
-Copy-Item .\default.aspx .\tos3 -force
+Copy-Item .\default.aspxx .\tos3 -force
 Copy-Item .\default.aspx.cs .\tos3 -force
 Copy-Item .\packages.config .\tos3 -force
 Copy-Item .\readme.md .\tos3 -force
