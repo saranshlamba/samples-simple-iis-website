940e44f373f096744a06a38870d794501c4832fb
Saransh Lamba <saranshlamba@gmail.com>
2017-11-01 14:48:32 -0700

    checking

--- uploadtos3.ps1
+++ uploadtos3.ps1
@@ -5,7 +5,7 @@ $date=Get-Date -Format yyyy_M_dd_H_m_ss
 Write-S3Object -BucketName s3demo2017 -Key tos3.$date.zip -File .\tos3.zip -ProfileName slamba -Region us-west-2
 Write-S3Object -BucketName s3demo2017 -Key demo.json -File .\demo.json -ProfileName slamba -Region us-west-2
 Set-AWSCredentials -ProfileName slamba
-Update-CFNStack -StackName sbxdemo -Capability CAPABILITY_IAM -Parameter @(
+Update-CFNStack -StackName disneydemo -Capability CAPABILITY_IAM -Parameter @(
 @{ParameterKey="CIDR"; UsePreviousValue="True"},@{ParameterKey="Email"; UsePreviousValue="True"},@{ParameterKey="InstallerPath"; ParameterValue="tos3.$date.zip"},
 @{ParameterKey="InstanceType"; UsePreviousValue="True"},@{ParameterKey="KeyPairName"; UsePreviousValue="True"},
 @{ParameterKey="MaximumInstances"; UsePreviousValue="True"},@{ParameterKey="MinimumInstances"; UsePreviousValue="True"},
